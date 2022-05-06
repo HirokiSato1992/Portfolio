643b5685641511e3f507c69da198ce21e0b0c98e
Hiroki Sato <hiroki.sato.19920918@gmail.com>
2022-05-06 21:13:56 +0900

    feat(*): install chakra-ui

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -24,7 +24,21 @@ const config: GatsbyConfig = {
       "path": "./src/pages/"
     },
     __key: "pages"
-  }]
+  }, {
+    resolve: '@chakra-ui/gatsby-plugin',
+    options: {
+      /**
+       * @property {boolean} [resetCSS=true]
+       * if false, this plugin will not use `<CSSReset />
+       */
+      resetCSS: true,
+      /**
+       * @property {boolean} [isUsingColorMode=true]
+       * if false, this plugin will not use <ColorModeProvider />
+       */
+      isUsingColorMode: true,
+    },
+  },]
 };
 
 export default config;
